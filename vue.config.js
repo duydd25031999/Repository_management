1ac9f94df3cad495c9a7264c7fd22eac53588598
duydd25031999 <45323184+DoDucDuyVietnam@users.noreply.github.com>
2019-11-10 15:07:38 +0700

    update deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/repository_management/' : '/',
   "transpileDependencies": [
     "vuetify"
-  ],
-  publicPath: process.env.NODE_ENV === 'production' ? '/repository_management/' : '/'
+  ]
 }
